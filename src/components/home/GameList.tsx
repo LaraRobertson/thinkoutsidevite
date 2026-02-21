73f20f753377d686646367bef5761c708d68cbd1
LaraRobertson <lara@lararobertson.com>
2026-02-21 10:23:57 -0500

    small changes and am retrying push

--- src/components/home/GameList.tsx
+++ src/components/home/GameList.tsx
@@ -87,35 +87,34 @@ export default function GameList(props: GameListProps) {
     return (
         <View id="game-list">
 
-            <Heading level={6} className="heading" marginBottom="5px">
-                GAMES ARE IN TESTING MODE<br />Please contact info@escapeout.games to report issues.
-            </Heading>
+
             {user && (
                 <>
-                <h3>Welcome {user?.signInDetails?.loginId}</h3>
-                <Button
-                    className="button button-small background-light show"
-                    onClick={() => handleMyStats()}
-                >
-                    My Stats
-                </Button>
+                    <h3>Welcome {user?.signInDetails?.loginId}</h3>
+                    <Button
+                        className="button button-small background-light show"
+                        onClick={() => handleMyStats()}
+                    >
+                        My Stats
+                    </Button>
                 </>
             )}
-            <View className={gameLocationCity ? "dark-background-link" : "green-background-link"} margin="10px auto 5px auto" textAlign="center" fontSize=".7em" padding="5px" lineHeight="1.1em">
-                    <Heading level={6} className="heading" marginBottom="5px" >
-                        {(((gameListByCity.length === 0)&&!gameLocationCity) && !loading) ? "Select Game City: Click on a city to see game locations" : `Game City: ${gameLocationCity}`}
-                        {(gameListByCity.length === 0)&&gameLocationCity ? " (no game locations)" : ` (${gameListByCity.length} games)`}
-                    </Heading>
+            <View className={gameLocationCity ? "dark-background-link" : "green-background-link"}
+                  margin="10px auto 5px auto" textAlign="center" fontSize=".7em" padding="5px" lineHeight="1.1em">
+                <Heading level={6} className="heading" marginBottom="5px">
+                    {(((gameListByCity.length === 0) && !gameLocationCity) && !loading) ? "Select Game City: Click on a city to see game locations" : `Game City: ${gameLocationCity}`}
+                    {(gameListByCity.length === 0) && gameLocationCity ? " (no game locations)" : ` (${gameListByCity.length} games)`}
+                </Heading>
             </View>
-            
-            <Flex direction="row" justifyContent="flex-start" alignItems="stretch" 
+
+            <Flex direction="row" justifyContent="flex-start" alignItems="stretch"
                   alignContent="flex-start" wrap="wrap" gap="1rem" className="select-game">
                 {cities.map((city) => (
                     <div key={city.id} className="city">
-                        <Flex direction="column" justifyContent="flex-start" alignItems="center" 
+                        <Flex direction="column" justifyContent="flex-start" alignItems="center"
                               alignContent="center" wrap="wrap" gap=".1rem">
-                            <Button 
-                                marginRight="5px" 
+                            <Button
+                                marginRight="5px"
                                 className="button"
                                 backgroundColor={(localStorage.getItem("gameLocationCity") === city.cityName) ? "#0b441d" : "#dae9c6"}
                                 color={(localStorage.getItem("gameLocationCity") === city.cityName) ? "#ffffff" : "#000000"}
@@ -130,21 +129,23 @@ export default function GameList(props: GameListProps) {
 
             {(gameListByCity.length > 0) && (
                 <View>
-                    <View className={gameLocationPlace ? "dark-background-link" : "green-background-link"} margin="10px auto 5px auto" textAlign="center" fontSize=".7em" padding="5px" lineHeight="1.1em">
+                    <View className={gameLocationPlace ? "dark-background-link" : "green-background-link"}
+                          margin="10px auto 5px auto" textAlign="center" fontSize=".7em" padding="5px"
+                          lineHeight="1.1em">
                         <Heading level={6} className="heading" marginBottom="5px" marginTop="0px">
                             Select Game Location:&nbsp;&nbsp; {gameLocationPlace}
                         </Heading>
                     </View>
 
 
-                    <Flex direction="row" justifyContent="flex-start" alignItems="stretch" 
+                    <Flex direction="row" justifyContent="flex-start" alignItems="stretch"
                           alignContent="flex-start" wrap="wrap" gap="1rem" className="select-game">
                         {gameLocationPlaceArray.map((place, index) => (
                             <div key={index} className="place">
-                                <Flex direction="column" justifyContent="flex-start" alignItems="center" 
+                                <Flex direction="column" justifyContent="flex-start" alignItems="center"
                                       alignContent="center" wrap="wrap" gap=".1rem">
-                                    <Button 
-                                        marginRight="5px" 
+                                    <Button
+                                        marginRight="5px"
                                         className="button"
                                         backgroundColor={(gameLocationPlace === place) ? "#0b441d" : "#dae9c6"}
                                         color={(gameLocationPlace === place) ? "#ffffff" : "#000000"}
@@ -161,7 +162,7 @@ export default function GameList(props: GameListProps) {
 
             {(gameListByCityPlace.length > 0) && (
                 <View>
-                    <Flex direction="row" justifyContent="space-between" alignItems="center" 
+                    <Flex direction="row" justifyContent="space-between" alignItems="center"
                           marginTop="15px" marginBottom="5px">
                         <Heading level={6} className="heading">
                             Games in {gameLocationPlace}:
@@ -169,8 +170,8 @@ export default function GameList(props: GameListProps) {
                         {/* not sure why this show/hide is here  - removed !hideGameLevels from below */}
                         <button className="hide" onClick={() => setHideGamesLevels(!hideGamesLevels)}>
                             {hideGamesLevels ?
-                                <DownIcon width={15} height={15} /> :
-                                <UpIcon width={15} height={15} />
+                                <DownIcon width={15} height={15}/> :
+                                <UpIcon width={15} height={15}/>
                             }
                         </button>
                     </Flex>
@@ -178,12 +179,17 @@ export default function GameList(props: GameListProps) {
                     <Flex direction="row" justifyContent="flex-start" alignItems="stretch"
                           alignContent="flex-start" wrap="wrap" gap="1rem" className="select-game">
                         {gameListByCityPlace.map((game) => (
-                            <GameCard key={game.id} game={game} setGameDetails={setGameDetails} hasPlayed={gamesIDUserPlayed.includes(game.id)} canPlay={gamesIDUser.includes(game.id)} />
+                            <GameCard key={game.id} game={game} setGameDetails={setGameDetails}
+                                      hasPlayed={gamesIDUserPlayed.includes(game.id)}
+                                      canPlay={gamesIDUser.includes(game.id)}/>
                         ))}
                     </Flex>
 
                 </View>
             )}
+            <Heading level={6} className="heading" marginBottom="5px">
+                GAMES ARE IN TESTING MODE<br/>Please contact info@escapeout.games to report issues.
+            </Heading>
         </View>
     );
 }
